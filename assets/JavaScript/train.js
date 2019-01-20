10eff38002b3835d2af6c51a3fd5a7d69992ac3e
Emilykradke <emilykradke@gmail.com>
2019-01-20 13:00:07 -0600

    comment

---
+++ assets/JavaScript/train.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBKTAq52HgRsW4J01nEv8rxRilSgDD3RXE",
+    authDomain: "trainschedulerhw0119.firebaseapp.com",
+    databaseURL: "https://trainschedulerhw0119.firebaseio.com",
+    projectId: "trainschedulerhw0119",
+    storageBucket: "trainschedulerhw0119.appspot.com",
+    messagingSenderId: "138117412519"
+  };
+  
+  firebase.initializeApp(config);
